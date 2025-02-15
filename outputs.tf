8f4e101c33cad5646c912f0f93377fc336402624
poornima <kannayyavaka@gmail.com>
2025-02-15 17:27:08 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@
 # output "subnets_info" {
 #   value = aws_subnet.public
 # }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnets_id" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
